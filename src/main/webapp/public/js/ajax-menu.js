59eae955756c10776614890e8b15e3283399e314
Martin Risseeuw <hello@martinr.nl>
2014-12-22 11:06:41 +0100

    update martin

--- src/main/webapp/public/js/ajax-menu.js
+++ src/main/webapp/public/js/ajax-menu.js
@@ -1,55 +1,54 @@
-<script>
-	$(function() {
-		String.prototype.decodeHTML = function() {
-			return $("<div>", {html: "" + this}).html();
-	};
-	var $main = $(".mainContent"),
+$(function() {
+	String.prototype.decodeHTML = function() {
+	return $("<div>", {html: "" + this}).html();
+};
+var $main = $(".mainContent"),
 
-	init = function() {	
-		jQuery(document).ready(function() {
-				jQuery('.preview').addClass("hidden").viewportChecker({
-						classToAdd: 'visible animated fadeIn',
-						offset: 100
-					 });
-		});
+init = function() {	
+	jQuery(document).ready(function() {
+			jQuery('.preview').addClass("hidden").viewportChecker({
+					classToAdd: 'visible animated fadeIn',
+					offset: 100
+				 });
+	});
 
-		// Do this when a page loads.
-		$('.mainNavigation li a').click(function() {
-			$('.mainNavigation li.current').removeClass('current');
-			$(this).closest('li').addClass('current');
-		});	
-		if(window.location.pathname === "/openthings/tutorial.php" || window.location.pathname === "/openthings/project.php" || window.location.pathname === "/openthings/user.php"){
-			UserRating();
-		}
-		else{}		
-		if(window.location.pathname === "/openthings/tutorials.php" || window.location.pathname === "/openthings/index.php"  || window.location.pathname === "/openthings/designers.php"){
-			$( ".mainContent" ).addClass( "overviewPage" );
-		}
-		else{
-			$( ".mainContent" ).removeClass( "overviewPage" );
-		}
-		if(window.location.pathname === "/openthings/standard.php"){
-			$( ".mainContent" ).addClass( "standardPage" );
-		}
-		else{
-			$( ".mainContent" ).removeClass( "standardPage" );
-		}
-	},
+	// Do this when a page loads.
+	$('.mainNavigation li a').click(function() {
+		$('.mainNavigation li.current').removeClass('current');
+		$(this).closest('li').addClass('current');
+	});	
+	if(window.location.pathname === "/openthings/tutorial.php" || window.location.pathname === "/openthings/project.php" || window.location.pathname === "/openthings/user.php"){
+		UserRating();
+	}
+	else{}		
+	if(window.location.pathname === "/openthings/tutorials.php" || window.location.pathname === "/openthings/index.php"  || window.location.pathname === "/openthings/designers.php"){
+		$( ".mainContent" ).addClass( "overviewPage" );
+	}
+	else{
+		$( ".mainContent" ).removeClass( "overviewPage" );
+	}
+	if(window.location.pathname === "/openthings/standard.php"){
+		$( ".mainContent" ).addClass( "standardPage" );
+	}
+	else{
+		$( ".mainContent" ).removeClass( "standardPage" );
+	}
+},
 
-	ajaxLoad = function(html) {
-		document.title = html
-			.match(/<title>(.*?)<\/title>/)[1]
-			.trim()
-			.decodeHTML();		
-
-		init();
-	},
-
-	loadPage = function(href) {
-		$main.load(href + " .mainContent>*", ajaxLoad);
-	};
+ajaxLoad = function(html) {
+	document.title = html
+		.match(/<title>(.*?)<\/title>/)[1]
+		.trim()
+		.decodeHTML();		
 
 	init();
+},
+
+loadPage = function(href) {
+	$main.load(href + " .mainContent>*", ajaxLoad);
+};
+
+init();
 
 	$(window).on("popstate", function(e) {
 		if (e.originalEvent.state !== null) {
@@ -69,4 +68,3 @@
 		}
 	});
 });
-</script>
