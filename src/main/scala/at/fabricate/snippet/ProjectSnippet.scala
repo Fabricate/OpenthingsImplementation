234be20fe54a511595828b28e26727ba150d2a63
Johannes Fischer <jfischer@cosy.sbg.ac.at>
2014-12-22 22:41:11 +0100

    added dynamic update of the comment list after comment submit, still a bug remains because with multiple submit s

--- src/main/scala/at/fabricate/snippet/ProjectSnippet.scala
+++ src/main/scala/at/fabricate/snippet/ProjectSnippet.scala
@@ -15,11 +15,18 @@ import at.fabricate.model.Comment
 import net.liftweb.http.SHtml
 import at.fabricate.model.User
 import net.liftweb.http.js.JsCmds
+import net.liftweb.http.RequestVar
+import net.liftweb.http.js.jquery.JqJsCmds._
+import net.liftweb.http.js.jquery.JqJE.JqId
+import net.liftweb.http.js.jquery.JqJE.JqAppend
+import net.liftweb.http.js.jquery.JqJE.JqAppendTo
+import net.liftweb.http.js.JE.JsRaw
+import net.liftweb.http.js.JsCmd
 
 
 
 object ProjectSnippet extends DispatchSnippet with Logger {
-  
+    
   def dispatch : DispatchIt = {
     case "list" => list(_)
     case "edit" => edit _
@@ -54,52 +61,70 @@ object ProjectSnippet extends DispatchSnippet with Logger {
      "#designerpage *" #> "View Item"
    }
    
-   private def bindCommentCSS(comment: Comment) : CssSel= {
-     "#commenttitle *" #> comment.title.asHtml &
-     "#commentauthor *" #> "Posted by: %s".format(comment.author.asHtml) &     
-     "#commentmessage *" #> comment.comment.asHtml
-   }
-   
-   
-   
     private def list:  CssSel =   
     // just a dummy implementation
 //   "#item" #> Project.findAll.map(project => bindListCSS(project))
    "#designer" #> Project.findAll.map(project => bindListCSS(project))
    
-  private def view :  CssSel  =  {
+  private def view (xhtml: NodeSeq) :  NodeSeq  =  {
     var commentTitle = ""
     var commentAuthor :  String = if (User.loggedIn_?){
       User.currentUser.map(user => "%s %s".format(user.firstName, user.lastName )).get
     } else ""
     var commentMessage = ""
-    var commentTemplate = ""
+    var commentTemplate = xhtml
+    var theTemplate = xhtml
+    val newComment: Comment = Comment.create
+    // get just the comment section
+    ("#comment" #> {(node : NodeSeq) => {
+      commentTemplate = node
+      Text("removed")}}).apply(theTemplate)
+//    println("template: "+theTemplate)
+//    println("template cssselected:" +commentTemplate )
+   
+   def bindCommentCSS(comment: Comment) : CssSel= {
+     "#commenttitle *" #> comment.title.asHtml &
+     "#commentauthor *" #> "Posted by: %s".format(comment.author.asHtml) &     
+     "#commentmessage *" #> comment.comment.asHtml
+   }
       
     def addNewComment(project : Project) = {
-      val newComment: Comment = Comment.create.commentedItem(project).
+      newComment.commentedItem(project).
+//      val newComment: Comment = Comment.create.commentedItem(project).
       title(commentTitle).
       author(commentAuthor).
       comment(commentMessage)
       newComment.save
     }
+    
     def bindNewCommentCSS(project : Project) : CssSel= {
      "#newcomtitle" #> SHtml.ajaxText(commentTitle, commentTitle = _ )&
      "#newcomauthor" #> SHtml.ajaxText(commentAuthor, commentAuthor = _ )&     
      "#newcommessage" #> SHtml.ajaxTextarea(commentMessage, commentMessage = _ ) & // rows="6"
      "#newcomsubmit [onclick]" #> SHtml.ajaxInvoke(() => {
-			            addNewComment(project)
-			            JsCmds.Alert("added comment "+commentTitle)
+			            if (addNewComment(project)) {
+			              JqId("comments") ~> JqAppend( bindCommentCSS(newComment)(commentTemplate) ) &
+			              JsCmds.Noop
+//			                  ("#comment" #> bindCommentCSS(newComment)).apply(commentTemplate))
+//			              JsRaw("$('#comments').append( '<li>Test</li>' );")
+//			              JsCmds.Alert("added comment "+commentTitle)
+
+			            }
+			            else
+			              JsCmds.Alert("adding comment '"+commentTitle+"' failed" )
 			          } )
-   }
+   } 
+
     S.param("id").get match {
-      case Project.FindByID(project) => "#dbcontent" #> {MapperBinder.bindMapper(project, {
+      case Project.FindByID(project) => { 
+        ("#dbcontent" #> { MapperBinder.bindMapper(project, {
      "#icon [src]" #> project.icon .url &
-     "#comment" #> { // how to save the template that is available here ???? 
-       project.comments.map(comment => bindCommentCSS(comment) )
-     } &
+     "#comment" #> project.comments.map(comment => bindCommentCSS(comment))  &
      "#newcomment" #> bindNewCommentCSS(project)
-   }) _}
-      case _ => ("*" #> Text("Object not found!") )
+   }) _ }
+     ).apply (xhtml)
+    }
+      case _ => Text("Object not found!")
     }
    }
 }
