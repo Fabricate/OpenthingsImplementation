17fdab2cc4682cd645c0a6f78de1335efbeae9c5
Johannes Fischer <jfischer@cosy.sbg.ac.at>
2014-12-27 15:42:28 +0100

    added a difficulty tag to the BaseRich Mapper Entity

--- src/main/scala/at/fabricate/model/BaseRichEntity.scala
+++ src/main/scala/at/fabricate/model/BaseRichEntity.scala
@@ -3,6 +3,9 @@ package model
 
 import net.liftweb.mapper.LongKeyedMapper
 import net.liftweb.mapper.IdPK
+import net.liftweb.mapper.MappedEnum
+
+import at.fabricate.lib.EnumWithDescriptionAndObject
 
 // this is the mapper type where every higher level object inherits from
 // project and tutorial are examples for that
@@ -17,6 +20,23 @@ with IdPK{
 	// TODO:
 	// add Licence Tag (one of many that are stored in a database)
 	// add a difficulty (one of many that comes from a list of string options)
+    object difficultyEnum extends EnumWithDescriptionAndObject[String] {
+	val kids = Value("Kids","icon-difficulty1")
+	val starter = Value("Starter","icon-difficulty2")
+	val average = Value("Average","icon-difficulty3")
+	val advanced = Value("Advanced","icon-difficulty4")
+	val expert = Value("Expert","icon-difficulty5")
+	val genius = Value("Genius","icon-difficulty6")
+	}
+  
+   /**Beschreibt Datenfeld für den Ersteller eines Projektes als Fremdschluessel fuer Relation zu User-Objekten*/
+  object difficulty extends MappedEnum(this, difficultyEnum  ){
+    private def findEnumValue(text : String ) : difficultyEnum.ExtendedValue = difficultyEnum.valueOf(text).getOrElse(difficultyEnum.Value("Not Found","icon-difficulty1"))
+    private def getWrapped = findEnumValue(get.toString).wrapped
+    private def getDescription = findEnumValue(get.toString).description
+    override def asHtml = <span class={getWrapped}></span>
+    override def buildDisplayList: List[(Int, String)] = enum.values.toList.map(a => (a.id, findEnumValue(a.toString).description))
+  }
 	// add Tag, Tool, ...
     // add field createdby, maybe (without display) also interesting for other types
 
--- src/main/scala/at/fabricate/model/Project.scala
+++ src/main/scala/at/fabricate/model/Project.scala
@@ -11,14 +11,12 @@ import scala.xml.{NodeSeq,Text}
 import java.util.Calendar
 import scala.xml.UnprefixedAttribute
 import scala.xml.Null
+import at.fabricate.lib.{EnumWithKeyAndValue, EnumWithStringKeyAndValue, EnumWithDescriptionAndObject }
  
 /**Meta(Kompagnion)-Objekt für die Projekt-Klasse. Enthaelt instanzuebergreifende Einstellungen.
 * @author Johannes Fischer **/
-//BEGIN(crud)
-object Project extends Project with BaseRichEntityMeta[Project] with AddRepositoryMeta[Project] 
-//END(crud)
-//with CRUDify[Long, Project] 
-{
+
+object Project extends Project with BaseRichEntityMeta[Project] with AddRepositoryMeta[Project] {
   
   
 
@@ -26,49 +24,13 @@ object Project extends Project with BaseRichEntityMeta[Project] with AddReposito
 //  override def dbTableName = "project"
   /**Anordnung der Eingabefelder in automatisch generierten Formularen (CRUDFify)*/
 //  override def fieldOrder = List(teaser, creationDate, byUserId)
-  /**Name des Menuepunktes für die Ansicht aller Objekte fuer CRUDify-Seiten*/
-//  override def showAllMenuName = S.?("projects")
-  /**Name des Menuepunktes für das Erstellen eines neuen Objekts auf CRUDify-Seiten*/
-//  override def createMenuName = S.?("create\u0020new\u0020project")
 
-  
-  // create a multipart upload form for Crudify
-//  override def _createTemplate = super._createTemplate %  	new UnprefixedAttribute("multipart",Text("yes"),Null)
-  
-//  override def _editTemplate = super._editTemplate %  	new UnprefixedAttribute("multipart",Text("yes"),Null)
-
-//  override def viewTemplate = 
-    
-    //LiftRules.loadResourceAsXml("/viewProject.html")  openOr <p>Error loading template</p>
-  
-  //BEGIN(pageWrapper)
-  /**Einbettung aller News-CRUD-Seiten in das default-Template mit Admin-Menue und Titelanzeige 
-  * eines "Erstellen"-Links*/
-  /*
-  override def pageWrapper (body: NodeSeq) = {
-    <lift:surround with="default" at="content">
-      <lift:embed what="admin_menu"/>
-      <h1><lift:Menu.title/></h1>
-       
-      <div id="formBox">
-        {body}
-      </div>
-    </lift:surround>
-  }
-  //END(pageWrapper)
-  */
-}//object
+}
 
 
 /**Beschreibt eine Projekt-Instanz
 * @author Johannes Fischer **/
 class Project extends BaseRichEntity[Project] with AddRepository[Project] {
-  
-      // define WithImage
-  
-  // override def works, val gives the known nullpointer exception
-  
-//      object FindByID extends ObjectById[Project](this)
 
 
   override def defaultIcon = "/public/images/noproject.jpg"
@@ -99,10 +61,59 @@ class Project extends BaseRichEntity[Project] with AddRepository[Project] {
       val currentUser : List[User] = User.currentUser.toList
       Full(currentUser.map(u => (u.id.get, u.lastName.get) ) )
     }
-    //  Full(User.allTeachers.map(u => (u.id.is, u.lastName.is)))
   }
-  //END(crudModify)
 
+  /*
+  object myEnum extends EnumWithDescription {
+  override var _values = List(("icon-difficulty2"->"Starter"), ("icon-difficulty3"->"Avarage"), ("icon-difficulty4"->"Advanced"), ("icon-difficulty5"->"Expert"), ("icon-difficulty6"->"Genius"))
+  }
+  * 
+  */
+//  class DifficultyValue(name: String, description: String) extends ValueWithDescription
+  
+  /*
+  MappedEnumWithDescription(this,myEnum)
+  
+    protected class MappedEnumWithDescription(obj : MapperType, theEnum: EnumWithDescriptionAndObject[String]) extends MappedEnum(obj, theEnum  ){
+  
+    
+//    val values = List(("icon-difficulty2"->"Starter"), ("icon-difficulty3"->"Avarage"), 
+ *    ("icon-difficulty4"->"Advanced"), ("icon-difficulty5"->"Expert"), ("icon-difficulty6"->"Genius")))
+
+    /*
+	  /**Genutzter Spaltenname in der DB-Tabelle*/
+    override def dbColumnName = "project_initiator"
+    
+      
+    /**Name des Datenfeldes für CRUD-Seiten*/
+    override def displayName = S.?("project\u0020initiator")
+    
+      
+    /**Darstellung des Feldes auf CRUD-Seiten. Anstelle der Id wird Nachname und Vorname des Autors
+     * angezeigt bzw. "k.A." für "keine Angabe", wenn es zu dieser User-Id keinen User gibt. */
+    override def asHtml = Text(User.find(this).map(_.fullName).openOr("k.A."))
+    
+        
+    /**Namen-Auswahlliste für CRUD-Seiten*/
+    override def validSelectValues: Box[List[(Long, String)]] = {
+      val currentUser : List[User] = User.currentUser.toList
+      Full(currentUser.map(u => (u.id.get, u.lastName.get) ) )
+    }
+    * 
+    */
+    override def asHtml = <span class={super.asHtml}></span>
+    
+    
+	/**
+	* Build a list for the select. Return a tuple of (String, String) where the first string
+	* is the id.string of the Value and the second string is the Text name of the Value.
+	*/
+	def buildDisplayList: List[(Int, String)] = theEnum.nameDescriptionList.map(item : EnumWithDescription => item)
+//	  enum.values.toList.map(a => (a.id, a.toString))
+  }
+  * 
+  */
+  
 
   /**Liefert das Meta-Objekt zur eigenen Modellklasse.*/
   def getSingleton = Project
