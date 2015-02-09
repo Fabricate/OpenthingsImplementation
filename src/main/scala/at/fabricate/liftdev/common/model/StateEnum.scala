f8ec2b892202950c20e5aa1dfcab0f19ae01b311
Johannes Fischer <jfischer@cosy.sbg.ac.at>
2015-02-09 16:09:53 +0100

    changed status - still untested

--- src/main/scala/at/fabricate/liftdev/common/model/StateEnum.scala
+++ src/main/scala/at/fabricate/liftdev/common/model/StateEnum.scala
@@ -7,14 +7,29 @@ import scala.xml.Elem
 	// add a difficulty (one of many that comes from a list of string options)
     object StateEnum extends EnumWithDescriptionAndObject[Elem] {
       
-      private def wrapSpanWithClass(theClass : String) : Elem = <span class={theClass}></span>
+//      private def wrapSpanWithClass(theClass : String) : Elem = <span class={theClass}></span>
     
-	val concept = Value("concept / idea / draft",wrapSpanWithClass("icon-indication-bar0"))
-	val dev_early = Value("early state of development",wrapSpanWithClass("icon-indication-bar1"))
-	val dev_advanced = Value("advanced state of development",wrapSpanWithClass("icon-indication-bar2"))
-	val evolved = Value("evolved (medium state)",wrapSpanWithClass("icon-indication-bar3"))
-	val advanced = Value("advanced",wrapSpanWithClass("icon-indication-bar4"))
-	val mature = Value("mature",wrapSpanWithClass("icon-indication-bar5"))
+    val stateText = "project is in"
+    val state1 = "concept / idea / draft state"
+    val state2 = "early state of development"
+    val state3 = "advanced state of development"
+    val state4 = "evolved (medium state)"
+    val state5 = "advanced state"
+    val state6 = "mature state"
+      
+        private def wrapSpanWithClass(theClass : String,theText: String) : Elem = 
+        <li class="left">
+			<span class={theClass}></span>
+			<h5>{stateText}</h5>
+			<h6>{theText}</h6>
+		</li>
+  
+	val concept = Value(state1,wrapSpanWithClass("icon-indication-bar0",state1))
+	val dev_early = Value(state2,wrapSpanWithClass("icon-indication-bar1",state2))
+	val dev_advanced = Value(state3,wrapSpanWithClass("icon-indication-bar2",state3))
+	val evolved = Value(state4,wrapSpanWithClass("icon-indication-bar3",state4))
+	val advanced = Value(state5,wrapSpanWithClass("icon-indication-bar4",state5))
+	val mature = Value(state6,wrapSpanWithClass("icon-indication-bar5",state6))
 	
 	val downToMatureList = mature  :: Nil
     val downToAdvanced = advanced   :: downToMatureList
