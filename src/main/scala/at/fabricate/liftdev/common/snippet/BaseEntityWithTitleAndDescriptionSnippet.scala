015e47bc12aed5d33fe73d80e5aae4469ab969dc
Johannes Fischer <jfischer@cosy.sbg.ac.at>
2017-03-23 11:07:00 +0100

    added the normal pagination and fixed problems

--- src/main/scala/at/fabricate/liftdev/common/snippet/BaseEntityWithTitleAndDescriptionSnippet.scala
+++ src/main/scala/at/fabricate/liftdev/common/snippet/BaseEntityWithTitleAndDescriptionSnippet.scala
@@ -38,7 +38,7 @@ import net.liftweb.common.Full
 import net.liftweb.common.Empty
 import lib.MatchString
 import model.BaseEntityWithTitleAndDescription
-import net.liftmodules.textile.TextileParser
+//import net.liftmodules.textile.TextileParser
 import java.util.Locale
 import net.liftweb.http.RedirectResponse
 import net.liftweb.common.Box
@@ -51,7 +51,10 @@ import scala.xml.Elem
 import net.liftweb.http.js.JsCmds.Replace
 import scala.xml.UnprefixedAttribute
 import scala.xml.Null
-import net.liftweb.http.PaginatorSnippet
+
+//import net.liftmodules.textile.TextileParser
+
+import at.fabricate.liftdev.common.lib.TextileParser
 
 
 abstract class BaseEntityWithTitleAndDescriptionSnippet[T <: BaseEntityWithTitleAndDescription[T]] extends CustomizedPaginatorSnippet[T] with DispatchSnippet with Logger {
@@ -309,7 +312,7 @@ abstract class BaseEntityWithTitleAndDescriptionSnippet[T <: BaseEntityWithTitle
     case "edit" => edit _
     case "create" => create _
     case "view" => view(_)
-    case "paginate" => paginate _
+    case "paginate" => paginate(_)
     case "paginatecss" => paginatecss(_)
   }
   
